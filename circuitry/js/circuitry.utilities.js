8a4e5a168f95b1dd534f2f4811266e33d1bb628a
J <jonah.ahvonen@gmail.com>
2015-12-08 21:24:29 +0200

    Circuitry component test (missing)

--- circuitry/js/circuitry.utilities.js
+++ circuitry/js/circuitry.utilities.js
@@ -15,5 +15,19 @@ var CUtils =
 	{
 	    var halfRange = (max - min) / 2;
 	    return min + halfRange + Math.cos(t) * halfRange;
+	},
+
+	getQueryVariable: function(variable)
+	{
+	       var query = window.location.search.substring(1);
+	       var vars = query.split("&");
+
+	       for (var i=0;i<vars.length;i++) 
+	       {
+	               var pair = vars[i].split("=");
+	               if(pair[0] == variable){return pair[1];}
+	       }
+
+	       return(false);
 	}
 }
