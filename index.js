6183a3d9a7cf10cef8a687af70138367ca469f31
J <jonah.ahvonen@gmail.com>
2015-12-08 15:01:37 +0200

    Implement better cross-browser requestAnimationFrame?

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ $(document).ready(function()
 {
 	console.log('Circuitry init');
 
+	if (!window.requestAnimationFrame) 
+	{
+		window.requestAnimationFrame = (function() 
+		{
+			return window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame || // comment out if FF4 is slow (it caps framerate at ~30fps: https://bugzilla.mozilla.org/show_bug.cgi?id=630127)
+			window.oRequestAnimationFrame ||
+			window.msRequestAnimationFrame ||
+			function(callback, element) 
+			{
+				window.setTimeout(callback, 1000 / 60);
+			};
+		})();
+	}
+
 	circuitry = new Circuitry($('#circ_container'));
 
 });
